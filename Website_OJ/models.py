1a03184b73d988cc3c882e495a541b1917758708
Deepanshu <deepanshu.sahu27@gmail.com>
2022-06-16 22:11:53 +0530

    commit

--- Website_OJ/models.py
+++ Website_OJ/models.py
@@ -1,4 +1,6 @@
 
+from ast import mod
+from xmlrpc.client import DateTime
 from django.db import models
 
 # Create your models here.
@@ -14,3 +16,21 @@ class Member(models.Model):
 
     def __str__(self):
         return self.fullname
+
+class Problem(models.Model):
+    problem_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    statement = models.CharField(max_length=2000)
+    difficulty = models.CharField(max_length=200)
+    solved_status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Test_cases(models.Model):
+    input_case = models.CharField(max_length=200)
+    output_case = models.CharField(max_length=200)
+
+class Submission(models.Model):
+    verdict = models.CharField(max_length=200)
+
