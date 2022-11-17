1761a0d95a0ffcdad812baf85479c9cac2ee3cb7
Deepanshu <deepanshu.sahu27@gmail.com>
2022-11-17 14:45:52 +0530

    making deployment ready

--- Website_OJ/views.py
+++ Website_OJ/views.py
@@ -11,12 +11,12 @@ from rest_framework.renderers import JSONRenderer
 import requests ,subprocess ,filecmp
 
 def home(request):
-    json_data = requests.get('http://127.0.0.1:8000/api').json()
+    json_data = Problem.objects.all()
+    print(json_data)
     return render(request,'home.html',{'json_data':json_data})
 
 def problem_statement(request,p_id):
-    json_data = requests.get('http://127.0.0.1:8000/api').json()
-    problem_data=json_data[p_id-1]
+    problem_data=Problem.objects.get(problem_id=p_id)
     verdict="0"
     if request.method == "POST":
         submitted_file=request.FILES[ "file"]
